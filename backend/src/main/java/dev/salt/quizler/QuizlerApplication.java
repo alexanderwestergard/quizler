4fb775be57f2621605d368f8473dabff9c0afe02
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-08 14:27:24 +0100

    Initialize Java project

---
+++ backend/src/main/java/dev/salt/quizler/QuizlerApplication.java
@@ -0,0 +1,13 @@
+package dev.salt.quizler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizlerApplication.class, args);
+	}
+
+}
