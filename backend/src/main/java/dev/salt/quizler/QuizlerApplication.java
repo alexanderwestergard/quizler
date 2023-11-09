cfd59f0524ae85a4fc034182d4b3d9a40d08865c
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 21:07:28 +0100

    Format code

--- backend/src/main/java/dev/salt/quizler/QuizlerApplication.java
+++ backend/src/main/java/dev/salt/quizler/QuizlerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuizlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizlerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizlerApplication.class, args);
+    }
 
 }
