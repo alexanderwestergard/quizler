4dfa5f5cc7175f5b54e89d701dafaf9a6fdb538a
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 08:51:29 +0100

    Implement GET methods to return data form repository

--- backend/src/main/java/dev/salt/quizler/domain/controllers/Controller.java
+++ backend/src/main/java/dev/salt/quizler/domain/controllers/Controller.java
@@ -1,18 +1,38 @@
 package dev.salt.quizler.domain.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.salt.quizler.domain.models.Quiz;
+import dev.salt.quizler.domain.repos.QuizRepository;
+import org.hibernate.dialect.unique.CreateTableUniqueDelegate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping
 public class Controller {
+    private final QuizRepository repo;
+
+    public Controller(QuizRepository repo) {
+        this.repo = repo;
+    }
 
 
     @GetMapping
-    public String getSomething() {
-        return "Here is something";
+    public ResponseEntity<List<Quiz>> getAllQuiz() {
+        return ResponseEntity.ok(repo.findAll());
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Quiz> getSpecificQuiz(@PathVariable Long id) {
+
+        var quiz = repo.findById(id).orElse(null);
+        if (quiz == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(quiz);
+    }
+
 }
