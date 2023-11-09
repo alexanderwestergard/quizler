eaf89c0bf39b36fdac8d8e414b29b668bbd43490
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 21:06:55 +0100

    Create function for posting new quiz to backend

--- frontend/src/app/functions.ts
+++ frontend/src/app/functions.ts
@@ -1,3 +1,5 @@
+import { Quiz } from '@/types';
+
 export const addScore = async (name: string, score: number, quizId: number) => {
   try {
     const requestOptions = {
@@ -10,6 +12,23 @@ export const addScore = async (name: string, score: number, quizId: number) => {
       'http://localhost:8080/' + quizId,
       requestOptions
     );
+
+    if (!response.ok) {
+      const body = await response.json();
+      throw new Error(body.error.details[0].message);
+    }
+  } catch (error) {}
+};
+
+export const addQuiz = async (quiz: Quiz) => {
+  try {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(quiz),
+    };
+
+    const response = await fetch('http://localhost:8080/', requestOptions);
 
     if (!response.ok) {
       const body = await response.json();
