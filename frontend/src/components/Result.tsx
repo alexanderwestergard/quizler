db2762e3387487059c474927acc80b1cf4702f8c
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 13:46:51 +0100

    Add link to home screen

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export const Result = (score: number[]) => {
   return (
     <div>
       Your result is: {score[0] + '/' + score[1]}
       <button>See leaderboard</button>
-      <button>Home</button>
+      <Link href="/">
+        <button>Home</button>
+      </Link>
     </div>
   );
 };
