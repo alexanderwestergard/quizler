59c75962dde98586f7df0f4ff504fd4b066da9de
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 13:57:59 +0100

    Add link to leaderboard

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Result = (score: number[]) => {
+  const pathname = usePathname();
   return (
     <div>
       Your result is: {score[0] + '/' + score[1]}
-      <button>See leaderboard</button>
+      <Link href={pathname + '/' + 'leaderboard'}>
+        <button>Leaderboard</button>
+      </Link>
       <Link href="/">
         <button>Home</button>
       </Link>
