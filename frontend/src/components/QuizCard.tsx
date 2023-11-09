e03226715ef828dbc76d6def0de48263ebb13ed2
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 09:24:11 +0100

    Add QuizCard component

---
+++ frontend/src/components/QuizCard.tsx
@@ -0,0 +1,19 @@
+import { Quiz } from '@/types';
+
+export const QuizCard = (quiz: Quiz) => {
+  return (
+    <div>
+      <h1>{quiz.id}</h1>
+      {quiz.questions.map((question, index) => {
+        return (
+          <article key={index}>
+            <h2>{question.question}</h2>
+            {question.answers.map((answer, index) => {
+              return <button key={index}>{answer.answer}</button>;
+            })}
+          </article>
+        );
+      })}
+    </div>
+  );
+};
