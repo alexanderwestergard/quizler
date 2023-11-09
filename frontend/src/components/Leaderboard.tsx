d1ef49cfafecb1f77836280e820b12494d9aefce
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 21:30:18 +0100

    Add Home button to Leaderboard

--- frontend/src/components/Leaderboard.tsx
+++ frontend/src/components/Leaderboard.tsx
@@ -1,16 +1,21 @@
 import { Quiz } from '@/types';
+import Link from 'next/link';
 
 export const Leaderboard = (quiz: Quiz) => {
-  console.log(quiz);
   return (
     <section>
+      <h1>Leaderboard</h1>
       {quiz.leaderboard.map((score, index) => {
         return (
           <article key={index}>
-            <p>{score.name + ' - ' + score.score}</p>
+            <h3>{score.name + ' - ' + score.score}</h3>
           </article>
         );
       })}
+
+      <Link href="/quiz">
+        <button>Home</button>
+      </Link>
     </section>
   );
 };
