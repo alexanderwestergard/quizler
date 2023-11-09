e4a6aab19ef5c329cbb77b29a99d39080048c480
Alexander <alexander.westergard@appliedtechnology.se>
2023-11-09 11:56:20 +0100

    Implement functionality for showing if answer is correct

--- frontend/src/components/QuizCard.tsx
+++ frontend/src/components/QuizCard.tsx
@@ -1,19 +1,41 @@
 import { Quiz } from '@/types';
+import { MouseEventHandler, useRef, useState } from 'react';
 
 export const QuizCard = (quiz: Quiz) => {
+  const [questions, setQuestions] = useState(quiz.questions);
+  const question = questions[0].question;
+  const answers = questions[0].answers;
+  const [correct, setCorrect] = useState(false);
+  const [wrong, setWrong] = useState(false);
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const index = event.currentTarget.getAttribute('data-index');
+    if (index === null) {
+      return null;
+    }
+    const isCorrect = answers[+index].correct;
+
+    if (isCorrect) {
+      setCorrect(true);
+    } else {
+      setWrong(true);
+    }
+  };
   return (
-    <article className='flex flex-col'>
+    <article>
       <h1>{quiz.id}</h1>
-      {quiz.questions.map((question, index) => {
-        return (
-          <article key={index}>
-            <h2>{question.question}</h2>
-            {question.answers.map((answer, index) => {
-              return <button key={index}>{answer.answer}</button>;
-            })}
-          </article>
-        );
-      })}
+      <article>
+        <h2>{question}</h2>
+        {answers.map((answer, index) => {
+          return (
+            <button onClick={handleClick} key={index} data-index={index}>
+              {answer.answer}
+            </button>
+          );
+        })}
+        {correct && <p>Correct!</p>}
+        {wrong && <p>Wrong!</p>}
+      </article>
     </article>
   );
 };
